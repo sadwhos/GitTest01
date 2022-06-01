83d6f5a36f7a7c348420867eb83f699ff9ecc15d
rhr <123@163.com>
2022-06-01 20:30:44 +0800

    test1

---
+++ src/main/java/com/atguigu/git/GitTest01.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @title: GitTest01
+ * @Author Tan
+ * @Date: 2022/6/1 20:26
+ * @Version 1.0
+ */
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("GitTest01");
+    }
+}
